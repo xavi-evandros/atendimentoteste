cc81e66e784490a7bd0932bb4931614926127490
Evandro Xavier <exavier@MacBook-Air-de-Evandro.local>
2024-06-18 22:23:22 -0300

    Criando o Projeto

---
+++ src/main/java/com/sula/atendimenteste/AtendimentesteApplication.java
@@ -0,0 +1,13 @@
+package com.sula.atendimenteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendimentesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendimentesteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sula/atendimenteste/AtendimentesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sula.atendimenteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
